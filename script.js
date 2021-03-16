ae0dc2b47d9665daaab5ea6a36d877f84af4d659
Dania <dania.liu@ocsbstudent.ca>
2021-03-16 19:46:09 +0000

    Added space at the end of JS code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//Adds a button which activates when hearing a click.
 document.getElementById('button').addEventListener ('click', displayMessage)
-
+//Connects the click to make a function, in this case show up an alert.
 function displayMessage () {
   alert("Hello, World!")
 }
